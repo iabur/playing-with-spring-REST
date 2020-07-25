fc7c14dc4410c3bf3b86e9fcaa2301a4af85a81c
iaburrahman <iaburrahman@gmail.com>
2020-07-25 13:17:57 +0600

    generate random public userId.

--- src/main/java/com/iabur/ws/springbootfirst/service/impl/UserServiceImpl.java
+++ src/main/java/com/iabur/ws/springbootfirst/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.iabur.ws.springbootfirst.service.impl;
 import com.iabur.ws.springbootfirst.io.entity.UserEntity;
 import com.iabur.ws.springbootfirst.io.repository.UserRepository;
 import com.iabur.ws.springbootfirst.service.UserService;
+import com.iabur.ws.springbootfirst.shared.Utils;
 import com.iabur.ws.springbootfirst.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto userDto) {
 
@@ -20,7 +24,9 @@ public class UserServiceImpl implements UserService {
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(userDto, userEntity);
 
-        userEntity.setEncryptedPassword("testPassword");
+        String publicUserId = utils.generateUserId(30);
+
+        userEntity.setEncryptedPassword(publicUserId);
         userEntity.setUserId("testId");
 
         UserEntity storedValue = userRepository.save(userEntity);
