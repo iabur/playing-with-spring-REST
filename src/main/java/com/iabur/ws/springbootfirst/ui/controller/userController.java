23149a4ab4dd9bdf8abbfae3f4a1ca755f389582
iaburrahman <iaburrahman@gmail.com>
2020-07-30 17:21:13 +0600

    adding xml and json both support

--- src/main/java/com/iabur/ws/springbootfirst/ui/controller/userController.java
+++ src/main/java/com/iabur/ws/springbootfirst/ui/controller/userController.java
@@ -6,6 +6,7 @@ import com.iabur.ws.springbootfirst.ui.model.request.UserDetailsRequestModel;
 import com.iabur.ws.springbootfirst.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +15,7 @@ public class userController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String id) {
         UserRest returnValue = new UserRest();
         UserDto userDto = userService.getUserByUserId(id);
@@ -22,11 +23,12 @@ public class userController {
         return returnValue;
     }
 
-    @PostMapping
+    @PostMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
         UserDto userDto = new UserDto();
         UserRest returnValue = new UserRest();
-        BeanUtils.copyProperties(userDetails,userDto);
+        BeanUtils.copyProperties(userDetails, userDto);
         UserDto createUser = userService.createUser(userDto);
         BeanUtils.copyProperties(createUser, returnValue);
         return returnValue;
