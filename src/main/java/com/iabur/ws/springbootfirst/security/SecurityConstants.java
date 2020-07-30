148e593d10a48100603627d06ebe59911d6b4e51
iaburrahman <iaburrahman@gmail.com>
2020-07-30 16:23:56 +0600

    reading token secret from property file

--- src/main/java/com/iabur/ws/springbootfirst/security/SecurityConstants.java
+++ src/main/java/com/iabur/ws/springbootfirst/security/SecurityConstants.java
@@ -1,9 +1,16 @@
 package com.iabur.ws.springbootfirst.security;
 
+import com.iabur.ws.springbootfirst.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "DSLJDSODFS";
+
+    public static String getTokenSecret()
+    {
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+        return appProperties.getTokenSecret();
+    }
 }
