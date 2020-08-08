2391cde36a9c2ab6d1ac658b8efcc2c255d0dd77
iaburrahman <iaburrahman@gmail.com>
2020-08-08 11:08:02 +0600

    Setup war(web application archive)

--- src/main/java/com/iabur/ws/springbootfirst/SpringbootfirstApplication.java
+++ src/main/java/com/iabur/ws/springbootfirst/SpringbootfirstApplication.java
@@ -2,11 +2,18 @@ package com.iabur.ws.springbootfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringbootfirstApplication {
+public class SpringbootfirstApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootfirstApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootfirstApplication.class, args);
